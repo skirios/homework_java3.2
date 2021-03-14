fc38a7f1c90d2bba8218cd71aa92771f867592b3
skirios <skiriosfoto@gmail.com>
2021-03-14 17:04:43 +0300

    добавлено деление на 100

--- BonusMoney.java
+++ BonusMoney.java
@@ -1,13 +1,12 @@
 public class BonusMoney {
     public static void main(String[] args) {
         int balance = 10000;
-        int replenishmentAmount = 110000;
+        int replenishmentAmount = 1000;
         int balanceAfter;
         if (replenishmentAmount >= 100000) {
             balanceAfter = (balance + replenishmentAmount + replenishmentAmount / 100) / 100;
         }
-        else {
-             balanceAfter = balance + replenishmentAmount;
+        else {balanceAfter = (balance + replenishmentAmount) / 100;
         }
         System.out.println("Баланс счета: " + balanceAfter);
     }
--- BonusMoney2.java
+++ BonusMoney2.java
@@ -1,8 +1,8 @@
 public class BonusMoney2{
     public static void main(String[] args) {
         int balance = 10000;
-        int replenishmentAmount = 150000;
-        int balanceAfter = balance + replenishmentAmount;
+        int replenishmentAmount = 15000;
+        int balanceAfter = (balance + replenishmentAmount) / 100;
         if (replenishmentAmount >= 100000) {
             balanceAfter = (balance + replenishmentAmount + replenishmentAmount / 100) / 100;
         }
